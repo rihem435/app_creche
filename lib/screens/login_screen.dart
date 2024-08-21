2f6d5e9e8e98249931e2a76f281a711ade00c94a
rihem <>
2024-08-21 11:30:57 +0100

    add image backgroundimage in loginscreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,56 +9,122 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const Text(
-            "Welcome Back!",
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 50,
+          left: 25,
+          bottom: 25,
+          right: 25,
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              "assets/images/background.jpg",
             ),
+            fit: BoxFit.cover,
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Email",
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-                borderSide: BorderSide(
-                  color: Colors.blue,
-                  width: 2,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-                borderSide: BorderSide(
-                  color: Colors.blue,
-                  width: 2,
-                ),
-              ),
-              prefixIcon: Icon(
-                Icons.email,
+        ),
+        child: Column(
+          children: [
+            const Text(
+              "Welcome Back!",
+              style: TextStyle(
                 color: Colors.blue,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Password",
-              border: OutlineInputBorder(),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Image.asset(
+              "assets/images/creche.png",
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Email",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: InkWell(
+                child: Text(
+                  "forgot password?",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                onTap: () {
+                  print('forgot password');
+                },
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFFD789),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    //   fontFamily: 'Jua',
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
